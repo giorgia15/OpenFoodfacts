73abeaf489278a85191d9dd84f684a67895e9b9f
giorgia15 <bozzetti.giorgia@iisgalvanimi.edu.it>
2022-04-06 11:54:32 +0000

    finire punto 2

--- OpenFoodfacts/src/app/app-routing.module.ts
+++ OpenFoodfacts/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { CaratteristicheComponent } from './caratteristiche/caratteristiche.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent},
+  { path: 'caratteristiche', component: CaratteristicheComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
